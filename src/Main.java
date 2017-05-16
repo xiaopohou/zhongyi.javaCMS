2e5c358311ebb11a0dc42bb6019921b971f496d1
laozhao <3@3.com>
2017-05-17 02:31:33 +0800

    c

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String name="laozhao";
+        Integer age=30;
+        System.out.print("hello "+name+" your age is "+age);
+        System.out.println("Hello World!");
+    }
+}
